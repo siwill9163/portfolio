49a54fde34fd4ed296d3bf1cc82b76835f15498b
siwill9163 <simon.williams1985@outlook.com>
2024-12-05 11:35:59 +0000

    inde

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
   mode: "jit",
   theme: {
     extend: {
